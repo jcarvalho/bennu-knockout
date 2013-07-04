1e39a00a103c3740ab42f7e59d788ed3534ba819
João Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-07-04 18:38:25 +0100

    Pager now accepts the object instead of its id

--- src/main/webapp/bennu-knockout/bennu-knockout.js
+++ src/main/webapp/bennu-knockout/bennu-knockout.js
@@ -39,19 +39,21 @@ define(['jquery', 'knockout', 'i18n!nls/messages'], function($, ko, messages) {
 	    }
 	};
 
+	var idCounter = 0;
+
 	ko.bindingHandlers.pager = {
 	    init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-		    var arrayName = ko.utils.unwrapObservable(valueAccessor());
-
 		    var contents = $(element).html();
 
-		    var pagerModel = viewModel[arrayName + '$_pager'] = {};
+		    var id = 'pager' + idCounter++;
+
+		    var pagerModel = viewModel[id + '$_pager'] = {};
 
 		    var itemsPerPage = 2;
 		    var currentPageObs = pages = ko.observable(1);
 		    pagerModel['currentPage'] = currentPageObs;
 		    pagerModel['pagedArray'] = ko.computed(function() {
-		    	var collection = ko.utils.unwrapObservable(viewModel[arrayName]);
+		    	var collection = ko.utils.unwrapObservable(valueAccessor());
 		    	var firstItemIndex = itemsPerPage * (currentPageObs() -1);
 		    	return collection.slice(firstItemIndex, firstItemIndex + itemsPerPage);
 		    });
@@ -70,9 +72,9 @@ define(['jquery', 'knockout', 'i18n!nls/messages'], function($, ko, messages) {
 		    }
 		    pagerModel['numPages'] = numPages;
 
-		    $(element).html('<div data-bind="with: ' + arrayName +'$_pager"><!-- ko foreach: pagedArray -->' + contents + '<!-- /ko --></div>');
+		    $(element).html('<div data-bind="with: ' + id +'$_pager"><!-- ko foreach: pagedArray -->' + contents + '<!-- /ko --></div>');
 
-		    $(element).append('<div data-bind="with: ' + arrayName +'$_pager">\
+		    $(element).append('<div data-bind="with: ' + id +'$_pager">\
 		    	<div class="pagination pagination-small">\
 		    		<ul>\
 		    			<li data-bind="css: { disabled: currentPage() == 1 }"><a href="#" data-bind="click: previous">&laquo;</a></li>\
