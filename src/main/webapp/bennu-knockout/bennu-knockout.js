f770c1687f4052f5afaec0eb46c900ec49d53229
João Carvalho <joao.pedro.carvalho@ist.utl.pt>
2013-07-04 18:57:06 +0100

    Wrap 'with' using comments

--- src/main/webapp/bennu-knockout/bennu-knockout.js
+++ src/main/webapp/bennu-knockout/bennu-knockout.js
@@ -52,7 +52,7 @@ define(['jquery', 'knockout', 'i18n!nls/messages'], function($, ko, messages) {
 		    var pagerModel = viewModel[id + '$_pager'] = {};
 
 		    var itemsPerPage = 2;
-		    var currentPageObs = pages = ko.observable(1);
+		    var currentPageObs = ko.observable(1);
 		    pagerModel['currentPage'] = currentPageObs;
 		    pagerModel['pagedArray'] = ko.computed(function() {
 		    	var firstItemIndex = itemsPerPage * (currentPageObs() -1);
@@ -73,15 +73,15 @@ define(['jquery', 'knockout', 'i18n!nls/messages'], function($, ko, messages) {
 		    }
 		    pagerModel['numPages'] = numPages;
 
-		    $(element).html('<div data-bind="with: ' + id +'$_pager"><!-- ko foreach: pagedArray -->' + contents + '<!-- /ko --></div>');
+		    $(element).html('<!-- ko with: ' + id +'$_pager --><!-- ko foreach: pagedArray -->' + contents + '<!-- /ko --><!-- /ko -->');
 
-		    $(element).append('<div data-bind="with: ' + id +'$_pager">\
+		    $(element).append('<!-- ko with: ' + id +'$_pager -->\
 		    	<div class="pagination pagination-small">\
 		    		<ul>\
 		    			<li data-bind="css: { disabled: currentPage() == 1 }"><a href="#" data-bind="click: previous">&laquo;</a></li>\
 		    			<li data-bind="css: { disabled: currentPage() == numPages() }"><a href="#" data-bind="click: next">&raquo;</a></li>\
 		    		</ul>\
-		    	</div></div>');
+		    	</div><!-- /ko -->');
 	    }
 	};
 
